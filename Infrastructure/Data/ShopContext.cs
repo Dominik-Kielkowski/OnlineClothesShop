6ae19dfd1ba3cf11a7859ecd5ed1ce34eb4955d8
Dominik-Kielkowski <dominik0kielkowski@gmail.com>
2023-11-13 23:03:35 +0100

    Add project files.

---
+++ Infrastructure/Data/ShopContext.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructue.Data
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
