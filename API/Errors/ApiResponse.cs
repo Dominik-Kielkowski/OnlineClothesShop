5ddc3d1499dc635666e11df979c5169a21a588ed
Dominik-Kielkowski <dominik0kielkowski@gmail.com>
2023-11-27 22:50:25 +0100

    IntroducingErrorHandling

---
+++ API/Errors/ApiResponse.cs
@@ -0,0 +1,26 @@
+﻿namespace API.Errors
+{
+    public class ApiResponse
+    {
+        public ApiResponse(int statusCode, string errorMessage = null)
+        {
+            StatusCode = statusCode;
+            ErrorMessage = errorMessage ?? GetDefaultMessageForStatusCode(statusCode);
+        }
+
+        public int StatusCode { get; set; }
+        public string ErrorMessage { get; set; }
+
+        private string GetDefaultMessageForStatusCode(int statusCode)
+        {
+            return statusCode switch
+            {
+                400 => "Bad Request",
+                401 => "Not Authorized",
+                404 => "Resource not found",
+                500 => "Server Error",
+                _ => null,
+            };
+        }
+    }
+}
